dfbcee0d6d6080f260373cd731b5ab620411df25
Javier RP <jjrodriguez14@espe.edu.ec>
2021-09-07 17:40:30 -0500

    Nueva funcion

---
+++ Carpeta_P/arch.js
@@ -0,0 +1,17 @@
+function saludar(){
+
+    alert("Buenas noches");
+}
+
+document.getElementById("saludando").addEventListener("click", saludar);
+
+//Agregando una nueva función
+let a= 5;
+let b = 8;
+function sumar(a,b){
+
+    console.log("La suma es " + (a+b))
+
+}
+console.log(sumar);
+
